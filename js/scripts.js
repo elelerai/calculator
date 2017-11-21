173077b78c385a51b84f919f27fb914ae90ecbbf
eliud <elelerai@gmail.com>
2017-11-21 20:53:51 +0300

    calculator: js code applied in the form to do subtraction

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
-var add = function(num1, num2) {
-  return num1 + num2;
-};
+var add = function(number1, number2) {
+   return number1 + number2;
+ };
 
-var num1 = parseInt(prompt("Enter a number:"));
-var num2 = parseInt(prompt("Enter another number:"));
-var result = add(num1, num2);
-alert(result);
+ var subtract = function(number1, number2) {
+   return number1 - number2;
+ };
+
+ var multiply = function(number1, number2) {
+   return number1 * number2;
+ };
+
+ var divide = function(number1, number2) {
+   return number1 / number2;
+ };
+
+ $(document).ready(function() {
+   $("form#subtract").submit(function(event) {
+     event.preventDefault();
+     var number1 = parseInt($("#subtract1").val());
+     var number2 = parseInt($("#subtract2").val());
+     var result = subtract(number1, number2);
+     $("#output").text(result);
+   });
+ });
